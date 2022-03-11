2cd83b3c8e190d0c1f7ff825f28420b0a0f1a9bc
Kirill Lerman <kirilllerman@gmail.com>
2022-03-11 23:09:32 +0800

    homework without 4 task(+ tests)

--- src/main/java/learning/numberOfElements.java
+++ src/main/java/learning/numberOfElements.java
@@ -6,6 +6,16 @@ public class numberOfElements {
          * 1. Задан массив целых чисел, нужно найти количество элементов, значение которых больше чем их индекс
          */
         int[] arr = {10, 15, 20, 2, 5, 1, 10, 2};
+        int[] arr1 = {1, 2, 3, 4, 3, 2, 1};
+        int[] arr2 = {1, 2, 3, 3, 2, 1};
+        numberOfElement(arr);
+        average(arr);
+        palindrome(arr);
+        palindrome(arr1);
+        palindrome(arr2);
+
+    }
+    static int numberOfElement(int [] arr) {
         int number = 0;
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] > i) {
@@ -13,43 +23,46 @@ public class numberOfElements {
             }
         }
         System.out.println("Number of elements: " + number);
+        return number;
+    }
 
         /**
          * 2. Задан массив целых чисел, нужно найти среднее значение элементов
          */
-
+    static int average(int [] arr) {
         int aver = 0;
         for (int j : arr) {
             aver += j;
         }
         int average = aver / arr.length;
         System.out.println("Average of elements: " + average);
-
+        return average;
+    }
         /**
          * 3. Задан массив целых чисел, нужно ответить является ли он палиндромом (например: 12321 -> true, 123321 -> true, 123 -> false)
+         * @return
          */
-        //int[] arr1 = {1, 2, 3, 4, 3, 2, 1};
-        int[] arr2 = {1, 2, 3, 3, 2, 1};
-        //int[] arr3 = {1, 2, 3, 3};
+    static boolean palindrome(int [] arr) {
         int result = 0;
         boolean isPalindrome = false;
-        for (int i = 0, j = arr2.length - 1; i < arr2.length; i++, j--) {
-            if (arr2[i] == arr2[j]) {
+        for (int i = 0, j = arr.length - 1; i < arr.length; i++, j--) {
+            if (arr[i] == arr[j]) {
                 result = 0;
             } else {
                 result++;
             }
         }
-        if (result == 0){
+        if (result == 0) {
             isPalindrome = true;
         }
         System.out.println(isPalindrome);
-
+        return isPalindrome;
+    }
         /**
          * 4. Задан массив целых чисел, нужно сдвинуть его элементы на N влево.
          * Например: arr = 1,2,3,4,5 , N = 2, результат 3,4,5,1,2
          */
-    }
 }
+
 
 
--- src/test/java/learning/ArrayTest.java
+++ src/test/java/learning/ArrayTest.java
@@ -2,7 +2,9 @@ package learning;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ArrayTest {
 
@@ -23,6 +25,34 @@ public class ArrayTest {
     @Test
     public void testEratosfen() {
         boolean[] eratosfen = Array2Examples.getEratosfen(12);
+    }
+
+    @Test
+    public void testNumberOfElements(){
+        int[] arr = {10, 15, 20, 2, 5, 1, 10, 2};
+        int number = numberOfElements.numberOfElement(arr);
+        assertEquals( 5, number);
+    }
+
+    @Test
+    public void testAverage(){
+        int[] arr = {10, 15, 20, 2, 5, 1, 10, 2};
+        int average = numberOfElements.average(arr);
+        int result = Arrays.stream(arr).sum() / arr.length;
+        assertEquals(average, result);
+    }
+
+    @Test
+    public void testPalindrome(){
+        int[] arr = {10, 15, 20, 2, 5, 1, 10, 2};
+        int[] arr1 = {1, 2, 3, 4, 3, 2, 1};
+        int[] arr2 = {1, 2, 3, 3, 2, 1};
+        boolean result = numberOfElements.palindrome(arr);
+        boolean result1 = numberOfElements.palindrome(arr1);
+        boolean result2 = numberOfElements.palindrome(arr2);
+        assertFalse(result);
+        assertTrue(result1);
+        assertTrue(result2);
     }
 
 }
